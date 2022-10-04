8db6d1f932e151f45169a9fedb5eca1a80a87a72
Nino Suffoletta <nbsuffoletta@gmail.com>
2022-10-03 20:04:25 -0400

    update

---
+++ controllers/thought-controller.js
@@ -0,0 +1,15 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+    // get all thoughts
+
+    // create thought
+
+    // update thought
+
+    // delete thougth
+
+    // add reply
+
+    // delete reply
+}
